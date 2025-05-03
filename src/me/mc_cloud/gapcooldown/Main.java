d3fddb0ffceb62067c2fcc657f86b56e57f0ded8
Detobel <detobel36@hotmail.fr>
2025-05-03 09:35:40 +0200

    Static is not useful everywhere...

--- src/me/mc_cloud/gapcooldown/Main.java
+++ src/me/mc_cloud/gapcooldown/Main.java
@@ -23,19 +23,14 @@ public class Main extends JavaPlugin {
 	 * Message config
 	 */
 	
-	public static final String CONFIG_HEADER = "GapCooldown version 2.0\n"
-			+ "";
+	private static final String CONFIG_HEADER = "GapCooldown version 2.0\n";
 	
-	public static Map<Material, Map<String, Long>> playerCooldowns = new HashMap<>();
-	public static Map<Material, Integer> itemCooldowns = new HashMap<>();
-	public static Map<PotionType, Integer> potionCooldowns = new HashMap<>();
-	public static Map<PotionType, Map<String, Long>> playerPotionCooldowns = new HashMap<>();
-	//public static Map<String, Long> enchantedGoldenAppleCooldowns = new HashMap<String, Long>();
-	//public static int ENCHANTED_GOLDEN_APPLE_COOLDOWN = 0;
-	//public static Map<String, Long> goldenAppleCooldowns = new HashMap<String, Long>();
-	//public static int GOLDEN_APPLE_COOLDOWN = 0;
+	public Map<Material, Map<String, Long>> playerCooldowns = new HashMap<>();
+	public Map<Material, Integer> itemCooldowns = new HashMap<>();
+	public Map<PotionType, Integer> potionCooldowns = new HashMap<>();
+	public Map<PotionType, Map<String, Long>> playerPotionCooldowns = new HashMap<>();
 	
-	public static final Material[] FOODS = {Material.ENCHANTED_GOLDEN_APPLE, Material.GOLDEN_APPLE, Material.APPLE, Material.BAKED_POTATO,
+	private final Material[] FOODS = {Material.ENCHANTED_GOLDEN_APPLE, Material.GOLDEN_APPLE, Material.APPLE, Material.BAKED_POTATO,
 			Material.BEETROOT, Material.BEETROOT_SOUP, Material.BREAD, Material.CARROT, Material.CHORUS_FRUIT, Material.COOKED_BEEF, Material.COOKED_CHICKEN, Material.COOKED_COD,
 			Material.COOKED_MUTTON, Material.COOKED_PORKCHOP, Material.COOKED_RABBIT, Material.COOKED_SALMON, Material.COOKIE, Material.DRIED_KELP,
 			Material.GLOW_BERRIES, Material.GOLDEN_CARROT, Material.HONEY_BOTTLE, Material.MELON_SLICE, Material.MUSHROOM_STEW, Material.POISONOUS_POTATO,
@@ -43,7 +38,7 @@ public class Main extends JavaPlugin {
 			Material.PORKCHOP, Material.RABBIT, Material.SALMON, Material.ROTTEN_FLESH, Material.SPIDER_EYE, Material.SUSPICIOUS_STEW, Material.SWEET_BERRIES,
 			Material.TROPICAL_FISH};
 	
-	public static final PotionType[] EFFECTS = PotionType.values();
+	private final PotionType[] EFFECTS = PotionType.values();
 	
 	
 	public static Main instance;
@@ -52,6 +47,22 @@ public class Main extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		instance = this;
+		registerConfig();
+		populateMapConfig();
+
+		// Register listener
+		new StartEat(this);
+		new EndEat(this);
+		new OnDeath(this);
+		
+		new UpdateChecker(this, 101358).getVersion(version -> {
+			if (!this.getDescription().getVersion().equals(version)) {
+				getLogger().info("There is a new update available (" + version + ")! Go to https://www.spigotmc.org/resources/gap-cooldown.101358/ to download the new version.");
+			}
+		});
+	}
+
+	private void registerConfig() {
 		FileConfiguration config = getConfig();
 		
 		config.set("cooldownTime", null); // Remove old config
@@ -64,11 +75,12 @@ public class Main extends JavaPlugin {
 		config.options().copyDefaults(true);
 		config.options().header(CONFIG_HEADER);
 		saveConfig();
-		
-		
+	}
+
+	private void populateMapConfig() {
 		for (Material material : FOODS) {
 			int cooldown = config.getInt(material.toString().toLowerCase() + ".cooldown");
-			if (cooldown != 0) {
+			if (cooldown > 0) {
 				playerCooldowns.put(material, new HashMap<String, Long>());
 				itemCooldowns.put(material, cooldown);
 			}
@@ -76,21 +88,11 @@ public class Main extends JavaPlugin {
 		
 		for (PotionType potionType : EFFECTS) {
 			int cooldown = config.getInt("potion." + potionType.toString().toLowerCase() + ".cooldown");
-			if (cooldown != 0) {
+			if (cooldown > 0) {
 				potionCooldowns.put(potionType, cooldown);
 				playerPotionCooldowns.put(potionType, new HashMap<String, Long>());
 			}
 		}
-		
-		new StartEat(this);
-		new EndEat(this);
-		
-		new UpdateChecker(this, 101358).getVersion(version -> {
-            if (!this.getDescription().getVersion().equals(version)) {
-                getLogger().info("There is a new update available (" + version + ")! Go to https://www.spigotmc.org/resources/gap-cooldown.101358/ to download the new version.");
-            }
-        });
 	}
-
 
 }
